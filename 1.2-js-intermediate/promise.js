d45d0c11604bf9b450c2bf5f0c5ada18446c55ec
AnandaIlyasa <anandailyasa23244@student.uns.ac.id>
2023-07-05 16:46:50 +0700

    add async await

--- 1.2-js-intermediate/promise.js
+++ 1.2-js-intermediate/promise.js
@@ -1,8 +1,8 @@
 function getSongList() {
     const songList = [
-        { title: "song title", artists: [{ name: "artist name 1"}], duration: 2000 },
-        { title: "song title", artists: [{ name: "artist name 1"}], duration: 2000 },
-        { title: "song title", artists: [{ name: "artist name 1"}], duration: 2000 },
+        { title: "all of the lights", artists: [{ name: "Kanye West"}, { name: "Rihanna"}], duration: 2000 },
+        { title: "Dancin", artists: [{ name: "Aaron Smith"}], duration: 2000 },
+        { title: "Pump It", artists: [{ name: "The Black Eyed Peas"}], duration: 2000 },
     ];
     return new Promise((resolve) => {
         setTimeout(() => {
@@ -13,9 +13,28 @@ function getSongList() {
 
 getSongList()
     .then((songList) => {
-        console.log('Available Songs :', songList);
+        console.log('My playlist:');
+        for(let i = 0; i < songList.length; i++) {
+            console.log(i+1, songList[i].title)
+        }
     })
     .catch((error) => {
         console.error('Error:', error);
     });
+
+const retrieveSongs = async () => {
+    const songList = await getSongList();
+    console.log("===============================")
+    console.log('My playlist:')
+    for(let i = 0; i < songList.length; i++) {
+        console.log(i+1, songList[i].title)
+    }
+}
+
+
+try {
+    retrieveSongs();
+} catch (error) {
+    console.log(error);
+}
   
