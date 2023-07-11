19d0886fa77eae29ecc87b1f2e130ba58f79f60e
AnandaIlyasa <anandailyasa23244@student.uns.ac.id>
2023-07-11 15:14:58 +0700

    simple auth and how express-session works

--- 2.1-nodejs/index.js
+++ 2.1-nodejs/index.js
@@ -1,31 +1,53 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 const session = require('express-session');
+const store = new session.MemoryStore();
+const adminList = ["admin1", "admin2"];
+
+app.use(express.urlencoded({extended: false}));
 
 app.use(
   session({
     secret: 'my_secret_key',
     resave: false,
     saveUninitialized: false,
+    store
   })
 );
 
 app.get('/', (req, res) => {
-  res.render('main.ejs');
-})
-
-app.get('/tes', (req, res) => {
-  res.send('ini tes');
-})
-
-app.use((req, res, next) => {
-  if (req.session.userId === undefined) {
-    console.log('Anda tidak login');
+  if (req.session.isLoggedIn) {
+    res.render('authenticated.ejs', {session: JSON.stringify(store)});
   } else {
-    console.log('Anda telah login');
+    res.redirect('/login');
   }
-  next();
+})
+
+app.get('/login', (req, res) => {
+  res.render('login.ejs', {message: ""});
+})
+
+app.post('/login', (req, res) => {
+  const user = req.body.user;
+  if (adminList.includes(user)) {
+    req.session.isLoggedIn = true;
+    req.session.username = user;
+    res.locals.data = "data dummy " + user;
+    res.redirect('/');
+  } else {
+    req.session.isLoggedIn = false;
+    res.render('login.ejs', {message: "Username salah"});
+  }
+})
+
+app.get('/logout', (req, res) => {
+  req.session.destroy((error) => {
+    if (error) {
+      console.log(error);
+    }
+    res.redirect('/login');
+  });
 });
 
 app.listen(port, () => {
