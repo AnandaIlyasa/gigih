2e3461976ecc5497bd7ea641fb00298312360d01
AnandaIlyasa <anandailyasa23244@student.uns.ac.id>
2023-07-12 00:10:42 +0700

    add comment and reference about express session

--- 2.1-nodejs/index.js
+++ 2.1-nodejs/index.js
@@ -5,6 +5,12 @@ const session = require('express-session');
 const store = new session.MemoryStore();
 const adminList = ["admin1", "admin2"];
 
+// Express session is not the same as session storage in web browser.
+// express session is stored in the server and the session id will be stored in the browser's cookie
+// by default session is stored in memory and is not designed for production
+// https://expressjs.com/en/resources/middleware/session.html#:~:text=Note%20Session%20data%20is%20not%20saved%20in%20the%20cookie%20itself%2C%20just%20the%20session%20ID.%20Session%20data%20is%20stored%20server%2Dside.
+// https://expressjs.com/en/resources/middleware/session.html#:~:text=Warning%20The%20default%20server%2Dside%20session%20storage%2C%20MemoryStore%2C%20is%20purposely%20not%20designed%20for%20a%20production%20environment.%20It%20will%20leak%20memory%20under%20most%20conditions%2C%20does%20not%20scale%20past%20a%20single%20process%2C%20and%20is%20meant%20for%20debugging%20and%20developing.
+
 app.use(express.urlencoded({extended: false}));
 
 app.use(
@@ -33,7 +39,6 @@ app.post('/login', (req, res) => {
   if (adminList.includes(user)) {
     req.session.isLoggedIn = true;
     req.session.username = user;
-    res.locals.data = "data dummy " + user;
     res.redirect('/');
   } else {
     req.session.isLoggedIn = false;
