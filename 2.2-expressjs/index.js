066074bbb64b045e09adf86a89d036a893004bb4
AnandaIlyasa <anandailyasa23244@student.uns.ac.id>
2023-07-12 22:38:04 +0700

    feat(2.2-expressjs): add simple spotify playlist server

---
+++ 2.2-expressjs/index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const songList = [
+    {
+        title: 'SloMo',
+        artists: ['Chanel'],
+        url: 'https://open.spotify.com/embed/track/3XREkzDHsWdBL5tybyCDBH?utm_source=generator',
+        playlist: 'My fav'
+    },
+    {
+        title: 'Don\'t start now',
+        artists: ['Dua Lipa'],
+        url: 'https://open.spotify.com/embed/track/3PfIrDoz19wz7qK7tYeu62?utm_source=generator',
+        playlist: 'hip hop'
+    },
+];
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: false}));
+
+app.get('/', (req, res) => {
+  const playlists = songList.map((song)=> song.playlist).filter((value, index, array) => array.indexOf(value) === index);
+  res.render('index.ejs', {songList, playlists});
+})
+
+app.get('/playlist/:name', (req, res) => {
+  const playlistSongs = songList.filter((song) => song.playlist === req.params.name);
+  res.render('playlist.ejs', {playlist: req.params.name, playlistSongs});
+})
+
+app.get('/song', (req, res) => {
+  const playlists = songList.map((song)=> song.playlist).filter((value, index, array) => array.indexOf(value) === index);
+  res.render('addSong.ejs', {playlists});
+})
+
+app.post('/song', (req, res) => {
+  const artists = req.body.artists.split(",");
+  const newSong = {...req.body, artists};
+  songList.push(newSong);
+  res.redirect('/');
+});
+
+app.get('/tes', (req, res) => {
+  const primitive = "ilyas";
+  res.json(primitive);
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
