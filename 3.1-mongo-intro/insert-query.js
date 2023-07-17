9a028893a5cf4d0ad9d49d8a0077601824e13cca
AnandaIlyasa <anandailyasa23244@student.uns.ac.id>
2023-07-18 02:10:23 +0700

    feat(3.1-mongodb-intro): add script to insert documents in mongodb

---
+++ 3.1-mongo-intro/insert-query.js
@@ -0,0 +1,220 @@
+db.songs.insertMany(
+    [
+        {
+            "title": "song 1",
+            "artists": [
+                "artist 1",
+                "artist 2"
+            ],
+            "album": "my album"
+        },
+        {
+            "title": "song 1",
+            "artists": [
+                "artist 1",
+                "artist 2"
+            ],
+            "album": "my album"
+        },
+        {
+            "title": "song 1",
+            "artists": [
+                "artist 1",
+                "artist 2"
+            ],
+            "album": "my album"
+        },
+        {
+            "title": "song 1",
+            "artists": [
+                "artist 1",
+                "artist 2"
+            ],
+            "album": "my album"
+        },
+        {
+            "title": "song 1",
+            "artists": [
+                "artist 1",
+                "artist 2"
+            ],
+            "album": "my album"
+        },
+        {
+            "title": "song 1",
+            "artists": [
+                "artist 1",
+                "artist 2"
+            ],
+            "album": "my album"
+        },
+        {
+            "title": "song 1",
+            "artists": [
+                "artist 1",
+                "artist 2"
+            ],
+            "album": "my album"
+        },
+        {
+            "title": "song 1",
+            "artists": [
+                "artist 1",
+                "artist 2"
+            ],
+            "album": "my album"
+        },
+        {
+            "title": "song 1",
+            "artists": [
+                "artist 1",
+                "artist 2"
+            ],
+            "album": "my album"
+        },
+        {
+            "title": "song 1",
+            "artists": [
+                "artist 1",
+                "artist 2"
+            ],
+            "album": "my album"
+        }
+    ]
+)
+
+db.artists.insertMany([
+    {
+        "name": "John Doe",
+        "date of birth": new Date('09-09-1999'),
+        "genre": [
+            "pop",
+            "hip hop"
+        ]
+    },
+    {
+        "name": "John Doe",
+        "date of birth": new Date('09-09-1999'),
+        "genre": [
+            "pop",
+            "hip hop"
+        ]
+    },
+    {
+        "name": "John Doe",
+        "date of birth": new Date('09-09-1999'),
+        "genre": [
+            "pop",
+            "hip hop"
+        ]
+    },
+    {
+        "name": "John Doe",
+        "date of birth": new Date('09-09-1999'),
+        "genre": [
+            "pop",
+            "hip hop"
+        ]
+    },
+    {
+        "name": "John Doe",
+        "date of birth": new Date('09-09-1999'),
+        "genre": [
+            "pop",
+            "hip hop"
+        ]
+    },
+    {
+        "name": "John Doe",
+        "date of birth": new Date('09-09-1999'),
+        "genre": [
+            "pop",
+            "hip hop"
+        ]
+    },
+    {
+        "name": "John Doe",
+        "date of birth": new Date('09-09-1999'),
+        "genre": [
+            "pop",
+            "hip hop"
+        ]
+    },
+    {
+        "name": "John Doe",
+        "date of birth": new Date('09-09-1999'),
+        "genre": [
+            "pop",
+            "hip hop"
+        ]
+    },
+    {
+        "name": "John Doe",
+        "date of birth": new Date('09-09-1999'),
+        "genre": [
+            "pop",
+            "hip hop"
+        ]
+    },
+    {
+        "name": "John Doe",
+        "date of birth": new Date('09-09-1999'),
+        "genre": [
+            "pop",
+            "hip hop"
+        ]
+    }
+])
+
+db.popularSongs.insertMany([
+    {
+        "title": "song 1",
+        "playCount": 9,
+        "period": 2000
+    },
+    {
+        "title": "song 1",
+        "playCount": 9,
+        "period": 2000
+    },
+    {
+        "title": "song 1",
+        "playCount": 9,
+        "period": 2000
+    },
+    {
+        "title": "song 1",
+        "playCount": 9,
+        "period": 2000
+    },
+    {
+        "title": "song 1",
+        "playCount": 9,
+        "period": 2000
+    },
+    {
+        "title": "song 1",
+        "playCount": 9,
+        "period": 2000
+    },
+    {
+        "title": "song 1",
+        "playCount": 9,
+        "period": 2000
+    },
+    {
+        "title": "song 1",
+        "playCount": 9,
+        "period": 2000
+    },
+    {
+        "title": "song 1",
+        "playCount": 9,
+        "period": 2000
+    },
+    {
+        "title": "song 1",
+        "playCount": 9,
+        "period": 2000
+    },
+])
