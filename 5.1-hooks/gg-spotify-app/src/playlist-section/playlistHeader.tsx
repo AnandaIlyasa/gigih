5a9841fda5a33335624ec65329569a1220bb7207
AnandaIlyasa <anandailyasa23244@student.uns.ac.id>
2023-08-06 02:45:21 +0700

    (5-react): remove gg-spotify

--- 5.1-hooks/gg-spotify-app/src/playlist-section/playlistHeader.tsx
+++
@@ -1,8 +0,0 @@
-export default function PlaylistHeader() {
-    return (
-        <div className="playlist-header">
-          <h2><span className="fa fa-stream icon"></span>Playlist</h2>
-          <span className="fa fa-plus icon"></span>
-        </div>
-    )
-}
